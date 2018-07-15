9dcfa3802256b2608d6c61aee2c1ec532e245a5b
Maja Markovic <markovicm9000@gmail.com>
2018-07-15 11:55:57 +0200

    zadatak8

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -1,12 +1,10 @@
 @extends('cars.layouts.master')
 
 @section('content')
-   
-        @foreach($cars as $car)
-        <div><a href='cars/{{$car->id}}'>{{ $car->title }}</a></div>
 
-        @endforeach
+    @foreach($cars as $car)
+        <div><a href='cars/{{$car->id}}'>{{ $car->title }}</a></div>
+    @endforeach
 
 @endsection
     
-
--- resources/views/cars/layouts/master.blade.php
+++ resources/views/cars/layouts/master.blade.php
@@ -1,42 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
-    <link rel=stylesheet href='../css/cars.css'>
-    <link rel=stylesheet href='css/cars.css'>
-    
-    <title>Document</title>
-</head>
-<body>
-   
-    <div class="blog-masthead">
-        <div class="container">
-            <nav class="nav blog-nav">
-                <a class="nav-link active" href="{{ url('cars') }}">Cars</a>
-                <a class="nav-link" href="{{ url('about') }}">About</a> 
-            
-                
-            </nav>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+        <link rel=stylesheet href='../css/cars.css'>
+        <link rel=stylesheet href='css/cars.css'>
+        <title>Document</title>
+    </head>
+    <body>
+        <div class="blog-masthead">
+            <div class="container">
+                <nav class="nav blog-nav">
+                    <a class="nav-link active" href="{{ url('cars') }}">Cars</a>
+                    <a class="nav-link" href="{{ url('about') }}">About</a> 
+                </nav>
+            </div>
         </div>
-    </div>
-    <div class="container">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 blog-main">
 
-      <div class="row">
-
-        <div class="col-sm-8 blog-main">
-
-        @yield('content')
-          
-
-        </div><!-- /.blog-main -->
-
-
-      </div><!-- /.row -->
-
-    </div><!-- /.container -->
-   
-</body>
+                    @yield('content')
+            
+                </div><!-- /.blog-main -->
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </body>
 </html>
